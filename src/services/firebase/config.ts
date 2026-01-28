a042b73d2288838bd13297eaf1f79325982de367
Vicente Matheus Collin Pedroso <130712234+vimathss@users.noreply.github.com>
2026-01-28 14:36:41 -0300

    🔒️ Remoção de Credenciais
    Removed sensitive Firebase configuration details.

--- src/services/firebase/config.ts
+++ src/services/firebase/config.ts
@@ -5,13 +5,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { getFirestore } from 'firebase/firestore'; // Importa o serviço do Firestor
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCdG3PmXj_9-rmlmbiWWgini64j8Ns3-eI",
-    authDomain: "eureka-d9dbc.firebaseapp.com",
-    projectId: "eureka-d9dbc",
-    storageBucket: "eureka-d9dbc.firebasestorage.app",
-    messagingSenderId: "907035568339",
-    appId: "1:907035568339:web:a8d6a9e84418d548bc0c26",
-    measurementId: "G-27XKWXCWEZ"
+    // Firebase Keys aqui
 };
 
 // Initialize Firebase
